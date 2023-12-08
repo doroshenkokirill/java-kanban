aa667d44b229b708b3754c76f2e0220d11f75804
Kirill <doroshenkokirill56@yandex.ru>
2023-12-08 10:01:20 +0300

    style: вынес размер истории как константу для читаемости

--- src/Manager/InMemoryHistoryManager.java
+++ src/Manager/InMemoryHistoryManager.java
@@ -9,8 +9,11 @@ public class InMemoryHistoryManager implements HistoryManager {
     private final List<Task> browsingHistory = new ArrayList<>();
     @Override
     public void add(Task task) { // наполнение истории
+        int historySize = 10;
         browsingHistory.add(task);
-        if (browsingHistory.size() > 10) browsingHistory.remove(0); // записей > 10 => удаляем 1-й элемент
+        if (browsingHistory.size() > historySize) {
+            browsingHistory.remove(0); // записей > 10 => удаляем 1-й элемент
+        }
     }
 
     @Override
